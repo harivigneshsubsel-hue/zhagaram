8f7379005519d3be387387a8d59044ca1a22c5a0
harivigneshsubsel-hue <harivignesh.subsel@gmail.com>
2026-09-25 00:37:10 +0530

    "Fix Vercel API module imports-v1

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,1 @@
-import "dotenv/config";
-
-import { handleEnquiryRequest } from "../server/index.ts";
-
-export default async function handler(req: any, res: any) {
-  return handleEnquiryRequest(req, res);
-}
+export { default } from "../server/index.ts";

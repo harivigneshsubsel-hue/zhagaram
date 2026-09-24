09037cfe3163429d07bb12d33f8c8c066f2a62eb
harivigneshsubsel-hue <harivignesh.subsel@gmail.com>
2026-09-25 00:29:39 +0530

    Fix Vercel API module resolution-v1

--- api/enquiry.ts
+++ api/enquiry.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 
-import { handleEnquiryRequest } from "../server/index";
+import { handleEnquiryRequest } from "../server/index.ts";
 
 export default async function handler(req: any, res: any) {
   return handleEnquiryRequest(req, res);
--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,7 @@
-export { default } from "../server/index";
+import "dotenv/config";
+
+import { handleEnquiryRequest } from "../server/index.ts";
+
+export default async function handler(req: any, res: any) {
+  return handleEnquiryRequest(req, res);
+}
